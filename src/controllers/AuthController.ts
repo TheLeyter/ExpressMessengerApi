f005c0bf9a6c05e9a3c153d7475b5061e492d98b
Vlad Lopatynets <v.lopatynets@ukr.net>
2021-11-25 16:32:01 +0200

    Add models

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,25 @@
 // import { User } from "src/models/User";
 import express from "express";
+import { User, UserModel } from "src/models/User";
 
 export class AuthController{
 
-    async register(request: express.Request, response: express.Response){
+    async register(request: express.Request<{}, {}, User>, response: express.Response){
         try {
-            response.json({name:"Vlad",userName:"Leyter"})
+            console.log(request.body)
+            const data = request.body
+
+            const user = new UserModel({
+                userName: data.userName,
+                email: data.email,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                password: data.password,
+            })
+
+            await user.save()
+
+            response.status(225).json(data.password)
         } catch (error) {
             console.log(error)
         }
